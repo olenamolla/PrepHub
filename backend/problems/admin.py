8ca646c6820706d00feea9ca49926c9b543bf17c
Olena Molla <133937893+olenamolla@users.noreply.github.com>
2025-11-07 14:43:02 -0800

    Register Problem model, admin panel setup, and migrations

--- backend/problems/admin.py
+++ backend/problems/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Problem
 
 # Register your models here.
+
+# making models accesible in the admin panel so I can add/view problems easily
+admin.site.register(Problem)
