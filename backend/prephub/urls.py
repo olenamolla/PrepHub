fc0a6ade0ed8af13072825dbd17949c1c9b60ba8
Olena Molla <133937893+olenamolla@users.noreply.github.com>
2025-11-11 20:13:39 -0800

    feat(api): add ProblemViewSet and API routing; add editorial notes; verified /api/problems works

--- backend/prephub/urls.py
+++ backend/prephub/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from problems.views import ProblemViewSet
+
+router = routers.DefaultRouter()
+router.register(r'problems', ProblemViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include(router.urls))
 ]
--- backend/problems/views.py
+++ backend/problems/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Problem
+from .serializers import ProblemSerializer
+
 
 # Create your views here.
+
+class ProblemViewSet(viewsets.ModelViewSet):
+    
+    queryset = Problem.objects.all()
+    serializer_class = ProblemSerializer
+    
