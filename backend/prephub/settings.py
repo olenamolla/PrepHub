877cdb7c8a1f0e9ee329e4be17f7f0c25972e7d8
Olena Molla <133937893+olenamolla@users.noreply.github.com>
2025-11-14 12:55:20 -0800

    Modified settings. Add more CORS origins

--- backend/prephub/settings.py
+++ backend/prephub/settings.py
@@ -57,6 +57,15 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
+    
+]
+
+ALLOWED_HOSTS = [
+    "localost",
+    "127.0.0.1",
+    "0.0.0.0"
 ]
 
 ROOT_URLCONF = "prephub.urls"
